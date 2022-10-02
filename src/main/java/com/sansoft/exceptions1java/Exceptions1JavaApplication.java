0c7f6ed2c9bbf1a2641bf95f8532a8e47bba6bfb
Thiago Santiago Melo <thiagosantiagomelo@gmail.com>
2022-10-02 19:34:42 -0300

    "First commit wite the builded project"

---
+++ src/main/java/com/sansoft/exceptions1java/Exceptions1JavaApplication.java
@@ -0,0 +1,13 @@
+package com.sansoft.exceptions1java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exceptions1JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exceptions1JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sansoft/exceptions1java/Exceptions1JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansoft.exceptions1java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exceptions1JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
